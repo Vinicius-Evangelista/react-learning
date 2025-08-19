7b21cc0e2f448c637f007fe61edd6cedc7ae77ad
Vinicius Evangelista <vini.evangelista@live.com>
2025-08-19 11:40:13 -0300

    learning: styling techniques

---
+++ alert/src/App.tsx
@@ -0,0 +1,12 @@
+import {Alert} from './Alert';
+import './App.css';
+
+export default function App() {
+    return (
+        <div >
+            <Alert heading="Success" closable>
+                Everything is really good!
+            </Alert>
+        </div>
+    );
+}
